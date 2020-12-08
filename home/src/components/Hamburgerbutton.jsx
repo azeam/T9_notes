9954add93df1e1ac16c831be39640608525203b9
AlexGstudios <69988168+AlexGstudios@users.noreply.github.com>
2020-12-08 19:36:38 +0100

    changing branch

---
+++ home/src/components/Hamburgerbutton.jsx
@@ -0,0 +1,9 @@
+import React, { Components } from 'React';
+
+class Hambergerbutton extends Components{
+    return(
+        
+    );
+}
+
+export default (Hambergerbutton);
---
+++ home/src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Button from './Button'
+import './Sidebar.css';
+
+class Sidebar extends Component {
+
+    render(){
+        return(
+            <div className="sidebar">
+                <Button ></Button>
+                
+            </div>
+        );
+    }
+}
+
+export default (Sidebar);
