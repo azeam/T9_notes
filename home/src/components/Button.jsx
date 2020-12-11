1a6b5f6a43948202958df03941cf2eb50cf6e488
SaErn <69966798+SaErn@users.noreply.github.com>
2020-12-11 19:53:08 +0100

    Update css and changes in components
    Made surrounding divs in component js-files into react tags. Removes unnecessary divs when page render

--- home/src/components/Button.jsx
+++ home/src/components/Button.jsx
@@ -4,9 +4,9 @@ import './Button.css';
 class Button extends Component {
     render(){
         return(
-            <div>
+            <>
                 <button className={this.props.className} onClick={this.props.onClick}><span id={this.props.id}></span>{this.props.label}</button>
-            </div>
+            </>
         );
     }
 }
--- home/src/components/Title.jsx
+++ home/src/components/Title.jsx
@@ -4,9 +4,9 @@ import './Title.css';
 class Title extends Component{
     render(){
         return(
-            <div>
+            <>
                 <h1 name={this.props.name} className={this.props.className}>{this.props.label}</h1>
-            </div>
+            </>
         );
     }
 }
